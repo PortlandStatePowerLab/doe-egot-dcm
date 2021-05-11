9589968e263cca268d564aa215a01741c94292b5
blusky-cloud <sspitzer@pdx.edu>
2021-05-11 20:49:50 +0000

    moved der simulator method defs inline for a header only ecs /include

--- src/ecs/include/ecs/der_simulator.hpp
+++ src/ecs/include/ecs/der_simulator.hpp
@@ -9,9 +9,35 @@ namespace der
 class DERSimulator 
 {
     public:
-        DERSimulator();
-        ~DERSimulator();
-        void TestRun();
+        DERSimulator()
+        {
+            world_.import<der_simulator_module>();
+        }
+        ~DERSimulator() {}
+        void TestRun()
+        {
+            int mins = 0;
+            std::cout << "DERSimulator::TestRun" << std::endl;
+            std::cout << "How many minutes would you like to simulate?" << std::endl;
+            std::cin >> mins;
+            srand(mins); //seed rand() generator 
+
+            //world_.set_target_fps(1);
+            for (int i = 0; i < mins; i++)
+            {
+                for (int i = 0; i < 60; i++)
+                {
+                    world_.progress();
+                } 
+                if (i % 5 == 0) //only print incrementally
+                {
+                    std::cout <<" Minute " << i << ": ";
+                    PrintSimpleDERImportEnergy();
+                    std::cout << std::endl;
+                }
+
+            } 
+        }
         void PrintSimpleDERImportEnergy()
         {
             auto q = world_.query<der::der_simulator_module::SimpleDER>();
--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,10 @@
 #include <ecs/sim_module.hpp>
 
 std::string g_program_path;
-void SimulateDER(); //prototype
+
+void SimulateDER(); //prototypes
 void RunHttpsTests(int argc, char **argv);
 void GetParentPath(char** arg);
-/*
-void GetParentPath(char** arg)
-{
-    g_program_path = arg[0];
-    std::size_t found = g_program_path.find_last_of("/\\");
-    g_program_path = g_program_path.substr(0,found);
-};
-*/
 
 int main(int argc, char **argv)
 {
@@ -30,60 +23,5 @@ int main(int argc, char **argv)
   std::cout << "argv: " << *argv << std::endl;
   std::cout << "Parent path: " << g_program_path << std::endl;
 
-/*
-  //HttpsClient client(g_program_path, "host.docker.internal", "8886");
-  CombinedHttpsClient combined_client(g_program_path);
-
-  std::cout << client.Get("/dcap") << std::endl;
-  std::cout << client.Get("/tm") << std::endl;
-  std::cout << client.Get("/edev") << std::endl;
-  
-std::string freq = R"(<?xml version="1.0" encoding="utf-8"?>
-<FlowReservationRequest href="http://uri1" xmlns="urn:ieee:std:2030.5:ns">
-  <mRID>0FB7</mRID>
-  <description>description1</description>
-  <version>0</version>
-  <creationTime>1</creationTime>
-  <durationRequested>0</durationRequested>
-  <energyRequested>
-    <multiplier>1</multiplier>
-    <value>-140737488355328</value>
-  </energyRequested>
-  <intervalRequested>
-    <duration>0</duration>
-    <start>1</start>
-  </intervalRequested>
-  <powerRequested>
-    <multiplier>1</multiplier>
-    <value>1</value>
-  </powerRequested>
-  <RequestStatus>
-    <dateTime>1</dateTime>
-    <requestStatus>0</requestStatus>
-  </RequestStatus>
-</FlowReservationRequest>)";
-
-  std::string test_dtm_msg = R"(    <message>
-        <from>DCM</from>
-        <to>DER</to>
-        <content>
-            <command>
-                <type>SHED</type>
-                <start>1617498512</start>
-                <duration>3.1415</duration>
-            </command>
-            <expect_response>1</expect_response>
-        </content>
-        <logged>1617498512</logged>
-    </message>)";
-   //auto thing = client.Post("/na", test_dtm_msg);
-   auto thing2 = combined_client.PostCombined("/na", test_dtm_msg);
-   
-   //std::cout << client.Get("/fres") << std::endl;
-   //std::cout << client.Get("/sdev") << std::endl;
-    //std::cout << response << std::endl;
-    //sep::DeviceCapability *dcap = new sep::DeviceCapability;
-    //xml::Parse(boost::beast::buffers_to_string(response.body().data()), dcap);
-*/
-    return 0;
+  return 0;
 }
