72e5d330c3dd3589f70defbc7b554b2f24f07a70
blusky-cloud <sspitzer@pdx.edu>
2021-05-13 02:10:00 +0000

    includes for modules

--- src/ecs/include/ecs/dcm_components_module.hpp
+++ src/ecs/include/ecs/dcm_components_module.hpp
@@ -5,10 +5,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <flecs.h>
+#include "der_sim_module.hpp"
+#include "der_components_module.hpp"
 
 namespace dcm
 {
+class dcm_components_module
+{
+    public:
 
+        dcm_components_module(flecs::world& world_) 
+        {
+            //Register module with world 
+            world_.module<dcm_components_module>();
+            world_.import<der::der_components>();
+        }
+};
 
 
 } // namespace dcm
--- src/ecs/include/ecs/dcm_systems_module.hpp
+++ src/ecs/include/ecs/dcm_systems_module.hpp
@@ -5,10 +5,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <flecs.h>
+#include "der_sim_module.hpp"
+#include "der_components_module.hpp"
+#include "dcm_components_module.hpp"
 
 namespace dcm
 {
 
+class dcm_systems_module
+{
+    public:
+
+        dcm_systems_module(flecs::world& world_) 
+        {
+            //Register module with world 
+            world_.module<dcm_systems_module>();
+            world_.import<der::der_components>();
+            world_.import<dcm_components_module>();
+        }
+};
 
 
 } // namespace dcm
