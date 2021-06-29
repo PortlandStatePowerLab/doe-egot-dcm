0fff024c39e1a5e8ca0a7df32b63aa21ea556f25
blusky-cloud <sspitzer@pdx.edu>
2021-06-29 22:02:42 +0000

    problems

---
+++ cea2045_receiver/cea2045_rcvr.cpp
@@ -0,0 +1,9 @@
+#include "include/cea2045_receiver/cea2045_rcvr.hpp"
+
+namespace dcm 
+{
+
+CTA2045Receiver::CTA2045Receiver() {}
+CTA2045Receiver::~CTA2045Receiver() {}
+
+} // namespace dcm
--- src/cta2045_receiver/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "include/cta2045_receiver/cta2045_receiver.hpp"
-
-int main(int argc, char **argv)
-{
-
-    return 0;
-}
