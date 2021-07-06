8e67db8719ffe137c01965ebea0057c2afa613a6
blusky-cloud <sspitzer@pdx.edu>
2021-07-06 03:35:33 +0000

    changed USB0 serial to AMA0

--- src/cta2045_receiver/cta2045_receiver.cpp
+++ src/cta2045_receiver/cta2045_receiver.cpp
@@ -4,30 +4,33 @@
 
 namespace dcm
 {
-
-CTA2045Receiver::CTA2045Receiver(): serial_port("/dev/ttyUSB0"), shutdown(false)
+using namespace cea2045;
+CTA2045Receiver::CTA2045Receiver(): serial_port("/dev/ttyAMA0"), shutdown(false)
 {
+    
     std::cout << "CTA2045 Receiver Default Constructor" << std::endl;
     if (!serial_port.open())
 	{
 		LOG(ERROR) << "failed to open serial port: " << strerror(errno);
 	}
-
-    device = cea2045::DeviceFactory::createUCM(&serial_port, &epri_ucm);
-    device->start();
-    timer.reset();
-    responseCodes = device->querySuportDataLinkMessages().get();
-	LOG(INFO) << "  query data link elapsed time: " << timer.getElapsedMS();
-	timer.reset();
-	responseCodes = device->queryMaxPayload().get();
-	LOG(INFO) << "  query max payload elapsed time: " << timer.getElapsedMS();
-	timer.reset();
-	responseCodes = device->querySuportIntermediateMessages().get();
-	LOG(INFO) << "  query intermediate elapsed time: " << timer.getElapsedMS();
-	timer.reset();
-	responseCodes = device->intermediateGetDeviceInformation().get();
-	LOG(INFO) << "  device info elapsed time: " << timer.getElapsedMS();
-	LOG(INFO) << "startup complete";
+    else
+    {
+        device = cea2045::DeviceFactory::createUCM(&serial_port, &epri_ucm);
+        device->start();
+        timer.reset();
+        responseCodes = device->querySuportDataLinkMessages().get();
+        LOG(INFO) << "  query data link elapsed time: " << timer.getElapsedMS();
+        timer.reset();
+        responseCodes = device->queryMaxPayload().get();
+        LOG(INFO) << "  query max payload elapsed time: " << timer.getElapsedMS();
+        timer.reset();
+        responseCodes = device->querySuportIntermediateMessages().get();
+        LOG(INFO) << "  query intermediate elapsed time: " << timer.getElapsedMS();
+        timer.reset();
+        responseCodes = device->intermediateGetDeviceInformation().get();
+        LOG(INFO) << "  device info elapsed time: " << timer.getElapsedMS();
+        LOG(INFO) << "startup complete";
+    }
 }
 
 CTA2045Receiver::~CTA2045Receiver() 
