4940870e97e14735addc598b87031d9a34810d54
blusky-cloud <sspitzer@pdx.edu>
2021-07-20 21:10:32 +0000

    more print

--- src/cta2045_receiver/ecs_epri_ucm_impl.cpp
+++ src/cta2045_receiver/ecs_epri_ucm_impl.cpp
@@ -134,6 +134,7 @@ void EPRI_UCM::processAckReceived(cea2045::MessageCode messageCode)
 		break;
 	}
 	outgoing = xml_writer_.WriteMsg("DER", "DCM", "AckReceived", "na", body);
+	std::cout << "about to POST from processAckReceived" << std::endl;
 	combined_client_->Post("DTM", outgoing);
 }
 
